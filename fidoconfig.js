03f9f1ab2a57ff348e3ea10a44709dc0f8033127
Mithgol the Webmaster <getgit@mithgol.ru>
2015-03-07 21:48:15 +0300

    initial approach to reading echomail area settings (no docs or tests)

--- fidoconfig.js
+++ fidoconfig.js
@@ -1,1 +1,54 @@
-module.exports = 'Hello world.';
+var extend = require('extend');
+var simteconf = require('simteconf');
+
+var strLeft = require('underscore.string/strLeft');
+
+var beforeSpace = function(inString){
+   if( inString.indexOf(' ') === -1 ){
+      return inString;
+   }
+   return strLeft(inString, ' ');
+};
+
+module.exports.areas = function(areaFilePath, givenOptions){
+   var defaultSettings = {
+      encoding: 'utf8'
+   };
+   var options = extend({}, defaultSettings, givenOptions);
+
+   var configLines = simteconf(areaFilePath, {
+      encoding: options.encoding,
+      skipNames: ['#'],
+      lowercase: false,
+      prefixGroups: [
+         'NetmailArea',
+         'BadArea',
+         'DupeArea',
+         'LocalArea',
+         'EchoArea'
+      ]
+   });
+   var echoArea = configLines.group('EchoArea');
+
+   var echomailAreas = {};
+   echoArea.names().forEach(function(echoAreaName){
+      var lcEchoAreaName = echoAreaName.toLowerCase();
+      var echoAreaLine = echoArea.first(echoAreaName);
+      var echoPath = beforeSpace(echoAreaLine);
+      var passthrough = ( echoPath.toLowerCase() === 'passthrough' );
+
+      var echoDesc = /-d "([^"]+?)"/.exec(echoAreaLine);
+      if( echoDesc !== null ){
+         echoDesc = echoDesc[1];
+      }
+
+      echomailAreas[lcEchoAreaName] = {
+         configName: echoAreaName,
+         passthrough: passthrough,
+         path: echoPath,
+         description: echoDesc
+      };
+   });
+
+   return echomailAreas;
+};
